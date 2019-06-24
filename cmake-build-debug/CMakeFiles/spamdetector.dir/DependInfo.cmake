109fd1969bfe58e862fdd592fc98e41bbf07c45e
Mujahid Omer <mjdmujahid.96@gmail.com>
2019-06-24 17:09:47 +0530

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/spamdetector.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/d/Mujahid/Desktop/MINOR Project/spamdetector/classifier.cpp" "/cygdrive/d/Mujahid/Desktop/MINOR Project/spamdetector/cmake-build-debug/CMakeFiles/spamdetector.dir/classifier.cpp.o"
+  "/cygdrive/d/Mujahid/Desktop/MINOR Project/spamdetector/converter.cpp" "/cygdrive/d/Mujahid/Desktop/MINOR Project/spamdetector/cmake-build-debug/CMakeFiles/spamdetector.dir/converter.cpp.o"
+  "/cygdrive/d/Mujahid/Desktop/MINOR Project/spamdetector/test_english.cpp" "/cygdrive/d/Mujahid/Desktop/MINOR Project/spamdetector/cmake-build-debug/CMakeFiles/spamdetector.dir/test_english.cpp.o"
+  "/cygdrive/d/Mujahid/Desktop/MINOR Project/spamdetector/trainning_data.cpp" "/cygdrive/d/Mujahid/Desktop/MINOR Project/spamdetector/cmake-build-debug/CMakeFiles/spamdetector.dir/trainning_data.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/spamdetector.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spamdetector.dir/classifier.cpp.o"
+  "CMakeFiles/spamdetector.dir/converter.cpp.o"
+  "CMakeFiles/spamdetector.dir/test_english.cpp.o"
+  "CMakeFiles/spamdetector.dir/trainning_data.cpp.o"
+  "spamdetector.pdb"
+  "spamdetector.exe"
+  "libspamdetector.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spamdetector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
